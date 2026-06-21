60d0cfb1110e29352c79d6aaf7817dd5b03060ea
Adeeb-Bahamid <adeebbahamid@gmail.com>
2026-06-22 00:14:38 +0300

    after changed simple4

--- core/utils/audio_extractor.py
+++ core/utils/audio_extractor.py
@@ -1,5 +1,5 @@
 #audio_extractor.py
 import subprocess
 def extract_audio(video_path, output_audio_path):
-    cmd = ['ffmpeg', '-y', '-i', video_path, '-vn', '-acodec', 'libmp3lame', '-ar', '16000', '-ac', '1','b:a','-64k', output_audio_path]
+    cmd = ['ffmpeg', '-y', '-i', video_path, '-vn', '-acodec', 'libmp3lame', '-ar', '16000', '-ac', '1','b:a','64k-', output_audio_path]
     subprocess.run(cmd, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
